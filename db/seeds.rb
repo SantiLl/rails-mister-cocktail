58370449b9a6e7f07a2d58b23ef1df7052edfe31
Santiago Llapur <santillapur@gmail.com>
2019-10-25 16:54:51 -0300

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Ingredient.destroy_all
+
+url = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredients_array = JSON.parse(url)
+
+puts "Adding ingredients!"
+ingredients_array["drinks"].each do |ingredient|
+  new_ingredient = Ingredient.new
+  new_ingredient.name = ingredient['strIngredient1']
+  new_ingredient.save
+end
+
+puts "Finished"
